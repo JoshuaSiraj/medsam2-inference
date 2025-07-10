906dfec5a136ec1a70e23318c2b5f3084ead67ca
Joshua Siraj <joshua.siraj@ryerson.ca>
2025-07-10 09:50:39 -0400

    init

---
+++ src/medsam2_inference/utils.py
@@ -0,0 +1,176 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def dice_multi_class(preds, targets):
+    smooth = 0.0 #1.0
+    assert preds.shape == targets.shape
+    labels = np.unique(targets)[1:]
+    dices = []
+    for label in labels:
+        pred = preds == label
+        target = targets == label
+        intersection = (pred * target).sum()
+        dices.append((2.0 * intersection + smooth) / (pred.sum() + target.sum() + smooth))
+    return np.mean(dices)
+
+def show_mask(mask, ax, mask_color=None, alpha=0.5):
+    """
+    show mask on the image
+
+    Parameters
+    ----------
+    mask : numpy.ndarray
+        mask of the image
+    ax : matplotlib.axes.Axes
+        axes to plot the mask
+    mask_color : numpy.ndarray
+        color of the mask
+    alpha : float
+        transparency of the mask
+    """
+    if mask_color is not None:
+        color = np.concatenate([mask_color, np.array([alpha])], axis=0)
+    else:
+        color = np.array([251/255, 252/255, 30/255, alpha])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_box(box, ax, edgecolor='blue'):
+    """
+    show bounding box on the image
+
+    Parameters
+    ----------
+    box : numpy.ndarray
+        bounding box coordinates in the original image
+    ax : matplotlib.axes.Axes
+        axes to plot the bounding box
+    edgecolor : str
+        color of the bounding box
+    """
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor=edgecolor, facecolor=(0,0,0,0), lw=2))     
+
+
+def resize_grayscale_to_rgb_and_resize(array, image_size):
+    """
+    Resize a 3D grayscale NumPy array to an RGB image and then resize it.
+    
+    Parameters:
+        array (np.ndarray): Input array of shape (d, h, w).
+        image_size (int): Desired size for the width and height.
+    
+    Returns:
+        np.ndarray: Resized array of shape (d, 3, image_size, image_size).
+    """
+    d, h, w = array.shape
+    resized_array = np.zeros((d, 3, image_size, image_size))
+    
+    for i in range(d):
+        img_pil = Image.fromarray(array[i].astype(np.uint8))
+        img_rgb = img_pil.convert("RGB")
+        img_resized = img_rgb.resize((image_size, image_size))
+        img_array = np.array(img_resized).transpose(2, 0, 1)  # (3, image_size, image_size)
+        resized_array[i] = img_array
+    
+    return resized_array
+def mask2D_to_bbox(gt2D, file):
+    try:
+        y_indices, x_indices = np.where(gt2D > 0)
+        x_min, x_max = np.min(x_indices), np.max(x_indices)
+        y_min, y_max = np.min(y_indices), np.max(y_indices)
+        # add perturbation to bounding box coordinates
+        H, W = gt2D.shape
+        bbox_shift = np.random.randint(0, 6, 1)[0]
+        scale_y, scale_x = gt2D.shape
+        bbox_shift_x = int(bbox_shift * scale_x/256)
+        bbox_shift_y = int(bbox_shift * scale_y/256)
+        x_min = max(0, x_min - bbox_shift_x)
+        x_max = min(W-1, x_max + bbox_shift_x)
+        y_min = max(0, y_min - bbox_shift_y)
+        y_max = min(H-1, y_max + bbox_shift_y)
+        boxes = np.array([x_min, y_min, x_max, y_max])
+        return boxes
+    except Exception as e:
+        raise Exception(f'error {e} with file {file} and sum of gts is {gt2D.sum()}')
+
+
+def mask3D_to_bbox(gt3D, file):
+    z_indices, _, _ = np.where(gt3D > 0)
+    z_min, z_max = np.min(z_indices), np.max(z_indices)
+    # add perturbation to bounding box coordinates
+    D, H, W = gt3D.shape
+
+    z_mid = (z_min + z_max) // 2
+    gt_mid = gt3D[z_mid]
+
+    box_2d = mask2D_to_bbox(gt_mid, file)
+    x_min, y_min, x_max, y_max = box_2d
+
+    z_min = max(0, z_min)
+    z_max = min(D-1, z_max)
+    boxes3d = np.array([x_min, y_min, z_min, x_max, y_max, z_max])
+    return boxes3d
+
+def preprocess(image_data, window_level=None, window_width=None):
+
+    lower_bound = window_level - window_width / 2
+    upper_bound = window_level + window_width / 2
+    image_data_pre = np.clip(image_data, lower_bound, upper_bound)
+    image_data_pre = (
+        (image_data_pre - np.min(image_data_pre))
+        / (np.max(image_data_pre) - np.min(image_data_pre))
+        * 255.0
+    )
+    #image_data_pre[image_data == 0] = 0
+    return image_data_pre
+
+def getEdgeOfMask(mask):
+    '''
+    Computes and returns edge of a segmentation mask
+    '''
+    # edge has the pixels which are at the edge of the mask
+    edge = np.zeros_like(mask)
+    
+    # mask_pixels has the pixels which are inside the mask of the automated segmentation result
+    mask_pixels = np.where(mask > 0)
+
+    for idx in range(0,mask_pixels[0].size):
+
+        x = mask_pixels[0][idx]
+        y = mask_pixels[1][idx]
+        z = mask_pixels[2][idx]
+
+        # Count # pixels in 3x3 neighborhood that are in the mask
+        # If sum < 27, then (x, y, z) is on the edge of the mask
+        if mask[x-1:x+2, y-1:y+2, z-1:z+2].sum() < 27:
+            edge[x,y,z] = 1
+            
+    return edge
+
+def AddedPathLength(auto, gt):
+    '''
+    Returns the added path length, in pixels
+    
+    Steps:
+    1. Find pixels at the edge of the mask for both auto and gt
+    2. Count # pixels on the edge of gt that are not in the edge of auto
+    '''
+    
+    # Check if auto and gt have same dimensions. If not, then raise a ValueError
+    if auto.shape != gt.shape:
+        raise ValueError('Shape of auto and gt must be identical!')
+
+    # edge_auto has the pixels which are at the edge of the automated segmentation result
+    edge_auto = getEdgeOfMask(auto)
+    # edge_gt has the pixels which are at the edge of the ground truth segmentation
+    edge_gt = getEdgeOfMask(gt)
+    
+    # Count # pixels on the edge of gt that are on not in the edge of auto
+    apl = (edge_gt > edge_auto).astype(int).sum()
+    
+    return apl 
